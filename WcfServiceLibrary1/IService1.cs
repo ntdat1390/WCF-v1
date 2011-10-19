365e13a501b18e5f0d076d8fc7e6df0b0722deae
The Dat <dat.nt1390@sinhvien.hoasen.edu.vn>
2011-10-19 11:43:08 +0700

    1234

---
+++ WcfServiceLibrary1/IService1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string GetAuthors();
+
+        // TODO: Add your service operations here
+    }
+    [ServiceContract]
+    public interface IService2
+    {
+        [OperationContract]
+        string GetAuthors2();
+
+        // TODO: Add your service operations here
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations
+}
